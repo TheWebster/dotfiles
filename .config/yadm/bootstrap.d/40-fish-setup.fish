7cbee910b3cd0e578b1558edb51f59bda28c760f
Christian Weber <vicus@mail.de>
2024-09-15 13:15:32 +0200

    modify fish bootstrap

--- .config/yadm/bootstrap.d/40-fish-setup.fish
+++ .config/yadm/bootstrap.d/40-fish-setup.fish
@@ -1,16 +1,6 @@
 #!/bin/fish
 
-function info
-    set_color -o
-    echo -n (status basename)": "
-    set_color normal
-    echo $argv
-end
-
-set -Ux fish_greeting
-
 # setup editor
-info "Setup editor..."
 set -l helix_path (command -s hx || command -s helix )
 
 if [ -n $helix_path ]
@@ -19,6 +9,4 @@ if [ -n $helix_path ]
     set -Ux VISUAL $helix_path
 end
 
-info "Install fisher plugins..."
-
-fisher update </dev/null
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher update
