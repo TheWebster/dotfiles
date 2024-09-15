74ea21ceb839d08751c1e49bd67d41f4b663b360
Christian Weber <vicus@mail.de>
2024-09-15 16:50:25 +0200

    added setup for rust-analyzer on artix

--- .config/yadm/bootstrap.d/40-fish-setup.fish
+++ .config/yadm/bootstrap.d/40-fish-setup.fish
@@ -1,12 +1,19 @@
 #!/bin/fish
 
+set -l distro (cat /etc/os-release | sed -En 's/^ID=(.+)$/\1/p')
+
+if [ "$distro" = artix ]
+    fish_add_path /usr/lib/rustup/bin/
+end
+
 # setup editor
 set -l helix_path (command -s hx || command -s helix )
 
 if [ -n $helix_path ]
-    info "Found helix at '$helix_path'"
     set -Ux EDITOR $helix_path
     set -Ux VISUAL $helix_path
 end
 
-curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher update
+if ! functions -q fisher
+    curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher update
+end
